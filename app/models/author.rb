410fb90e4b75a2bc4c906968b5c437e471f3c39c
derrick b <dbozich1@gmail.com>
2018-08-17 17:29:11 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ActiveRecord::Base
+  validates :name, length: { minimum: 2 }, uniqueness: true
+  # validates :bio, length: { maximum: 500 }
+  # validates :password, length: { in: 6..20 }
+  validates :phone_number, length: { is: 10 }
+  # validates :email, uniqueness: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,42 @@
+
+
 class Post < ActiveRecord::Base
+
+
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Non-Fiction Fiction), message: "%{value} is not a valid category" }
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+  /Won't Believe/i,
+  /Secret/i,
+  /Top [0-9]*/i,
+  /Guess/i
+]
+
+def is_clickbait?
+  if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+    errors.add(:title, "must be clickbait")
+  end
+end
+
+  #
+  # clickbait_patterns = [
+  #   /Won't Believe/i,
+  #   /Secret/i,
+  #   /Top [0-9]*/i,
+  #   /Guess/i
+  # ]
+  #
+  # def is_clickbait?
+  #   if clickbait_patterns.none? {|pat| pat.match title}
+  #     errors.add(:title, "must be clickbait")
+  #   end
+  # end
+  # validates :password, length: { in: 6..20 }
+  # validates :phone_number, length: { is: 10 }
+  # validates :email, uniqueness: true
 end
