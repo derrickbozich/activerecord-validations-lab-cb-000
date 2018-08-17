d5fb5c05db5fcdfcff51d4bd6cfa2a12a18cfdfc
derrick b <dbozich1@gmail.com>
2018-08-17 17:36:16 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,9 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, includes: w%(Fiction Non-Fiction)
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction),
+  message: "%{value} is not a valid category" }
+  
   
   
 end
