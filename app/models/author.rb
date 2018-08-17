67012d45f26482fffbc10fb72e639784d8e6775d
derrick b <dbozich1@gmail.com>
2018-08-17 17:31:04 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,4 @@
 class Author < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
-validates :b, length: { maximum: 500 }
-validates :password, length: { in: 6..20 }
-validates :phone_number, length: { is: 10 }
+  validates :phone_number, length: { is: 10 }
 end
